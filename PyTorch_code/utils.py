1d6eced91aa48d89780dd687153aedab1b1fc95c
Jyotin Goel <b22ai063@iitj.ac.in>
2024-07-07 23:55:04 +0530

    Unlearning for any and all models with k% change
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- PyTorch_code/utils.py
+++ PyTorch_code/utils.py
@@ -1,5 +1,5 @@
 from torch.autograd import grad
-from typing import List
+from typing import List, Dict
 from torch import Tensor
 import torch.nn as nn
 import torch
@@ -43,7 +43,7 @@ model = SimpleANN().to(device=device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-# Training function
+
 def train(model, train_loader, criterion, optimizer, epochs=5):
     model.train()
     for epoch in range(epochs):
@@ -57,7 +57,7 @@ def train(model, train_loader, criterion, optimizer, epochs=5):
             running_loss += loss.item()
         print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
 
-# Testing function
+
 def test(model, test_loader):
     model.eval()
     correct = 0
@@ -71,7 +71,6 @@ def test(model, test_loader):
             correct += (predicted == labels).sum().item()
     print(f"Accuracy: {100 * correct / total}%")
 
-# Train and test the model
 
 def compute_sensitivities_wrt_input(
     model: torch.nn.Module, 
@@ -81,35 +80,100 @@ def compute_sensitivities_wrt_input(
     Function to get weight sensitivity wrt input for any and all models.
     """
     outputs = model(x)
-    weights_model = []
     
-    for name, param in model.named_parameters():
-        if not param.requires_grad:
-            continue
-        if name.split('.')[1] == "weight":
-            print(name, param)
-            weights_model.append(param)
-    
+    weights_model = get_model_weights(model=model)
+
     all_classes_grad = []
-    for w_x in weights_model:
+    for _, w_x in weights_model.items():
         grad_f = grad(
-            outputs=outputs[0], 
+            outputs=outputs, 
             inputs=w_x, 
-            grad_outputs=torch.ones_like(outputs[0]), 
+            grad_outputs=torch.ones_like(outputs), 
             retain_graph=True
         )
         all_classes_grad.append(grad_f[0])  
 
     sensitivites = []
-    for i, x in enumerate(weights_model):
-        sensitivites.append(torch.mul(x, all_classes_grad[i]))
+    for i, weight in enumerate(weights_model.items()):
+        _, w_x = weight
+        sensitivites.append(torch.mul(w_x, all_classes_grad[i]))
 
     print(len(sensitivites))
     return sensitivites
 
+
+def convertToMask(tensor_sample: Tensor) -> Tensor:
+    z = torch.where(tensor_sample != -100, 1., 0.)
+    return z
+
+
+def top_k_sensi_tensor(sensi: Tensor, k: int) -> Tensor:
+    sensi = torch.abs(sensi)
+    l = int(k*sensi.shape[0]/100)
+    print(l)
+    desc = torch.argsort(-1*sensi, dim=1)
+    indexes = []
+
+    for x in desc:
+        col_index = []
+        for y in x:
+            col_index.append(y)
+            if len(col_index) == l:
+                break
+        indexes.append(col_index)
+
+    sensi_copy = torch.clone(sensi)
+    
+    for i in range(len(indexes)):
+        for j in indexes[i]:
+            sensi_copy[i][j] = -100
+
+    return sensi_copy
+
+
+def get_final_masks(sensi: List[Tensor], k: int) -> List[Tensor]:
+    top_k = []
+
+    for x in sensi:
+        top_k.append(top_k_sensi_tensor(sensi=x, k=k))
+    
+    top_k_masks = []
+    for x in top_k:
+        top_k_masks.append(convertToMask(x))
+
+    return top_k_masks
+
+
+def update_model_weights(model: nn.Module, masks: List[Tensor]) -> nn.Module:
+    i = 0
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue
+        if name.split('.')[1] == "weight":
+            param.data = torch.multiply(masks[i], param.data)
+            i += 1
+    
+    return model
+
+def get_model_weights(model: torch.nn.Module) -> Dict[str, Tensor]:
+    weights_model = {}
+
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue
+        if name.split('.')[1] == "weight":
+            weights_model[name.split('.')[0]] = param
+    
+    return weights_model
+
+
 if __name__ == "__main__":
-    # train(model, train_loader, criterion, optimizer, epochs=5)
-    # test(model, test_loader)
+    train(model, train_loader, criterion, optimizer, epochs=5)
+    test(model, test_loader)
+    prev_checkpt = "mnist.pt"
+
+    torch.save(model, prev_checkpt)
+
     for x, y in train_dataset:
         save_x = x.to(device)
         save_y = y
@@ -119,6 +183,10 @@ if __name__ == "__main__":
     save_x.requires_grad = True
     
     all_grad = compute_sensitivities_wrt_input(model=model, x=save_x)
-    # print(len(all_grad))
-    print(all_grad)
+    final_masks = get_final_masks(sensi=all_grad, k=30)
+    model_new = update_model_weights(model=model, masks=final_masks)
+    model_old = torch.load(prev_checkpt)
+    
+    test(model=model_old, test_loader=test_loader)
+    test(model=model_new, test_loader=test_loader)
 
